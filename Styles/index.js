ded8398632a90ef7015977e4b0af2ed633bb65d3
NoothangiBharathi <noothangi.bharathi1999@gmail.com>
2024-09-24 22:15:45 +0530

    JavaScriptLearning

---
+++ Styles/index.js
@@ -0,0 +1,33 @@
+// alert("Welcome to all");
+// a="Apple";
+// console.log("my name is Bharathi");
+// console.log(a);
+
+
+
+
+
+ function add() {
+   var a = parseInt(document.f1.val1.value);
+   var b = parseFloat(document.f1.val2.value);
+   var res = a + b;
+   document.f1.res.value = res;
+ }
+ function sub() {
+   var a = parseInt(document.f1.val1.value);
+   var b = parseFloat(document.f1.val2.value);
+   var res = a - b;
+   document.f1.res.value = res;
+ }
+ function mul() {
+   var a = parseInt(document.f1.val1.value);
+   var b = parseFloat(document.f1.val2.value);
+   var res = a * b;
+   document.f1.res.value = res;
+ }
+ function mod() {
+   var a = parseInt(document.f1.val1.value);
+   var b = parseFloat(document.f1.val2.value);
+   var res = a % b;
+   document.f1.res.value = res;
+ }
