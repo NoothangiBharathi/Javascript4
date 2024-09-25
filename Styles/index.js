898eff3b0b20beef337d30275a1271eb0dfedd41
NoothangiBharathi <noothangi.bharathi1999@gmail.com>
2024-09-25 22:12:39 +0530

    java script learning

--- Styles/index.js
+++ Styles/index.js
@@ -7,27 +7,126 @@
 
 
 
- function add() {
-   var a = parseInt(document.f1.val1.value);
-   var b = parseFloat(document.f1.val2.value);
-   var res = a + b;
-   document.f1.res.value = res;
- }
- function sub() {
-   var a = parseInt(document.f1.val1.value);
-   var b = parseFloat(document.f1.val2.value);
-   var res = a - b;
-   document.f1.res.value = res;
- }
- function mul() {
-   var a = parseInt(document.f1.val1.value);
-   var b = parseFloat(document.f1.val2.value);
-   var res = a * b;
-   document.f1.res.value = res;
- }
- function mod() {
-   var a = parseInt(document.f1.val1.value);
-   var b = parseFloat(document.f1.val2.value);
-   var res = a % b;
-   document.f1.res.value = res;
- }
+//  function add() {
+//    var a = parseInt(document.f1.val1.value);
+//    var b = parseFloat(document.f1.val2.value);
+//    var res = a + b;
+//    document.f1.res.value = res;
+//  }
+//  function sub() {
+//    var a = parseInt(document.f1.val1.value);
+//    var b = parseFloat(document.f1.val2.value);
+//    var res = a - b;
+//    document.f1.res.value = res;
+//  }
+//  function mul() {
+//    var a = parseInt(document.f1.val1.value);
+//    var b = parseFloat(document.f1.val2.value);
+//    var res = a * b;
+//    document.f1.res.value = res;
+//  }
+//  function mod() {
+//    var a = parseInt(document.f1.val1.value);
+//    var b = parseFloat(document.f1.val2.value);
+//    var res = a % b;
+//    document.f1.res.value = res;
+//  }
+
+
+
+
+
+
+
+
+
+
+
+
+// const openNav = document.getElementById("open")
+// const closeNav = document.getElementById("close")
+
+// const sideNavBar = document.querySelector(".sidenav")
+
+// closeNav.addEventListener('click', closeSideNavBar)
+
+// function closeSideNavBar() {
+//     sideNavBar.classList.toggle("mango")
+// }
+
+
+// const darkTheme = function() {
+//     document.querySelector("body").style.backgroundColor = "black"
+//     document.querySelector(".dark-theme").textContent = "Light Theme"
+//     document.querySelector("header").style.backgroundColor = "#2E282A"
+//     document.querySelector(".sidenav").style.backgroundColor = "#808A9F"
+// }
+
+// const lightTheme = function() {
+//     document.querySelector("body").style.backgroundColor = "white"
+//     document.querySelector(".dark-theme").textContent = "Dark Theme"
+//     document.querySelector("header").style.backgroundColor = "#F86624"
+//     document.querySelector(".sidenav").style.backgroundColor = "#1768AC"
+
+// }
+
+// function eleHover(element) {
+//     element.style.backgroundColor = "yellow";
+// }
+
+// function eleOut(item) {
+//     item.style.backgroundColor = "#DEE7E7"
+// }
+
+// const changeCompany = function() {
+//     document.querySelector("#company").textContent = "Sales Force";
+// };
+
+// const normal = function() {
+//     document.querySelector("#company").textContent = "Company"
+// }
+
+// const keyPressed = function() {
+//     document.querySelector("body").style.backgroundColor = "white"
+// }
+
+// const keyDown = function() {
+//     document.querySelector("body").style.backgroundColor = "red"
+// }
+
+
+
+
+
+
+
+
+// let games= ["Apples", "mango", 55,{name="lucky"}]
+
+
+// console.log(Array.isArray(games))
+
+
+
+
+// var a=30;
+// var b=20;
+
+
+// document.write("Result:" ,a+b);
+
+
+
+
+
+var game="Cricket";
+
+let captain="Dhoni";
+
+
+
+if(game==Cricket){
+
+  document.write("caption is Dhoni");
+}
+
